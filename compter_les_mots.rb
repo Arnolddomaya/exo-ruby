2f4bf79665deeafa3381daa2139c4f51d0963098
arnold-domaya <domaya_arnold@yahoo.fr>
2018-01-16 11:20:13 +0100

    ajout 'compterles mots'

--- compter_les_mots.rb
+++ compter_les_mots.rb
@@ -9,8 +9,29 @@ def jean_michel_data(strs="jaquar below how go",dico= ["below", "down", "go","go
       end
     end
   end
-  puts res
+  return res
 end
 
+def get_file_as_string(filename)
+  data = ''
+  f = File.open(filename, "r")
 
-jean_michel_data()
+  f.each_line do |line|
+    data <<line
+  end
+  return data
+end
+
+william_prose = get_file_as_string 'william.txt'
+william_prose=william_prose.split()
+
+dictionnary = ["the", "of", "and", "to", "a", "in", "for", "is", "on", "that", "by", "this", "with", "i", "you", "it", "not", "or", "be","are"]
+
+#resultat_will = jean_michel_data dictionnary.join(" "), william_prose
+#puts "le resultat de la recherche sur le bouquin de will a donné : #{resultat_will}"
+#jean_michel_data()
+
+mots_bizards = get_file_as_string 'motsBizard.txt'
+puts mots_bizards
+resultat_bizard = jean_michel_data mots_bizards.split(), william_prose
+puts "Gros mots dans l'oeuvre de Sheaks : #{resultat_bizard}"
